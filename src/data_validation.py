a7215831b74bd8981bf8f4a236fd05b18b2a62b9
erykml <eryk.lewinson@gmail.com>
2022-06-26 20:04:45 +0200

    bugfix

--- src/data_validation.py
+++ src/data_validation.py
@@ -2,7 +2,7 @@ import pandas as pd
 import glob 
 import os
 
-from config import RAW_DIR, PROCESSED_DIR, REPORTS_DIR
+from config import RAW_DIR, PROCESSED_DIR, REPORTS_DIR, AUGMENTED_DIR
 
 from deepchecks.tabular import Dataset
 from deepchecks.tabular.suites import full_suite
@@ -11,9 +11,9 @@ from deepchecks.tabular.suites import full_suite
 os.makedirs(REPORTS_DIR, exist_ok=True)
 
 # get the names of the resampled datasets
-data_files = glob.glob("data/processed/*.csv")
+data_files = glob.glob(f"{AUGMENTED_DIR}/*.csv")
 
-resampled_names = [file_name.replace("data/processed/", "").replace("X_train_", "").replace(".csv", "") for file_name in data_files if "X_train_" in file_name]
+resampled_names = [file_name.replace(f"{AUGMENTED_DIR}/", "").replace("X_train_", "").replace(".csv", "") for file_name in data_files if "X_train_" in file_name]
 resampled_names = [name for name in resampled_names if len(name) > 0]
 
 # loading the original data
@@ -25,8 +25,8 @@ ds_train = Dataset(X_train, label=y_train, cat_features=[])
 
 for resampled_data in resampled_names:
 
-    X_res = pd.read_csv(f"{PROCESSED_DIR}/X_train_{resampled_data}.csv", index_col=None)
-    y_res = pd.read_csv(f"{PROCESSED_DIR}/y_train_{resampled_data}.csv", index_col=None)
+    X_res = pd.read_csv(f"{AUGMENTED_DIR}/X_train_{resampled_data}.csv", index_col=None)
+    y_res = pd.read_csv(f"{AUGMENTED_DIR}/y_train_{resampled_data}.csv", index_col=None)
 
     # creating a deepchecks dataset from the resampled data
     ds_train_res = Dataset(X_res, label=y_res, cat_features=[])
