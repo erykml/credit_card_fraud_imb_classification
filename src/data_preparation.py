fd2c01b0ee16269a275cba99ea5f8106d8490f16
erykml <eryk.lewinson@gmail.com>
2022-06-26 14:36:27 +0200

    changed the random state

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -16,7 +16,7 @@ X = X.drop(columns=["Time"])
 
 # stratified train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y,  
-                                                    random_state=43,
+                                                    random_state=42,
                                                     test_size=0.2,  
                                                     stratify=y) 
 
